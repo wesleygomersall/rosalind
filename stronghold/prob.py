3476002b8aeb8729d0595425d2ed45a4541b11a9
Wesley Gomersall <gomersallw@gmail.com>
2025-02-27 12:41:56 -0800

    prob of seq with given gc content matching given seq

--- stronghold/prob.py
+++ stronghold/prob.py
@@ -3,6 +3,7 @@
 # https://rosalind.info/problems/prob/
 
 import argparse
+import math
 
 def get_args():
     parser = argparse.ArgumentParser(description="")
@@ -16,3 +17,22 @@ with open(args.input, 'r') as fin:
         if i == 0: seq = line.strip()
         if i == 1: arr = line.split()
 
+b_arr = []
+
+for gc in arr: 
+    g = c = 0.5 * float(gc)
+    a = t = 0.5 * (1 - float(gc)) 
+
+    prob = 1
+    for base in seq: 
+        match base: 
+            case "A": prob = prob * a 
+            case "T": prob = prob * t 
+            case "C": prob = prob * c 
+            case "G": prob = prob * g 
+
+    b_arr.append(math.log(prob, 10))
+
+for index, p in enumerate(b_arr): 
+    if index == len(b_arr) - 1: print(f"{p:.{3}f}") 
+    else: print(f"{p:.{3}f}", end=' ') 
