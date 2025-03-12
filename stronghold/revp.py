c13f3345ec8a9fc1f108f459373be35dbacd98e1
Wesley Gomersall <gomersallw@gmail.com>
2025-03-12 02:41:04 -0700

    WIP

--- stronghold/revp.py
+++ stronghold/revp.py
@@ -5,9 +5,32 @@
 import argparse
 import rostools
 
-def main(args):
-    sequence = rostools.read_fasta(args.fasta)[0]
+def revcomp(sequence: str) -> str: 
+    '''Returns the reverse complement of a DNA string
+    '''
+    comp = ""
 
+    for i in seq: 
+    match i: 
+        case "A":
+            comp = comp + "T" 
+        case "T":
+            comp = comp + "A" 
+        case "C":
+            comp = comp + "G" 
+        case "G":
+            comp = comp + "C" 
+    return comp[::-1]
+
+def main(args):
+    dna = rostools.read_fasta(args.fasta)[0]
+
+    for i in range(dna.length): 
+        expand = 0
+        while True: 
+            if dna.seq[i - expand] == rev_complement(dna.seq[i + 1 + expand])
+                    
+            dna.seq[i]
     # for sequence, return the position and length
     # of every reverse palindrome
 
